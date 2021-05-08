b8bb4b5489af46d857c66b4ebc659386966d0dee
Matt Jarjoura <jarjoura@gmail.com>
2021-05-08 11:10:41 -0700

    [android] moving to chart playground

---
+++ chartplayground/src/main/java/com/github/eklipse2k8/compose/chartplayground/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.github.eklipse2k8.compose.chartplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.eklipse2k8.compose.chartplayground.ui.theme.ComposechartTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ComposechartTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  ComposechartTheme {
+    Greeting("Android")
+  }
+}
